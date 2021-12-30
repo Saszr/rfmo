f8b5c4af2619b810f8234c4826fdd1e4aa9e1db7
Saszr <saszrlsh@gmail.com>
2021-12-30 11:38:24 +0800

    📦  feat: rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,23 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
+import visualizer from 'rollup-plugin-visualizer';
+
+const plugins = [];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  );
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({})],
+  plugins: [...plugins, react(), VitePWA({})],
   base: './',
   resolve: {
     alias: {
